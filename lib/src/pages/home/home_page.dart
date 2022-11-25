513c89089ddb997e2b511cfc7d6bc6668ee7e2fb
Rafael Ebrahim <rafael.ebrahim@gmail.com>
2022-11-25 19:09:34 -0300

    fix i

--- lib/src/pages/home/home_page.dart
+++ lib/src/pages/home/home_page.dart
@@ -11,7 +11,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final HomeController controller = GetIt.instance.get();
+  final HomeController controller = GetIt.I.get();
 
   @override
   void initState() {
